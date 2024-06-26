ea4eedc52997ffda8f6930d243c2356020f8e481
Peter Ndiba <ndibapeter4@gmail.com>
2024-06-26 10:38:58 +0300

    cmakelist

--- launch/cartographer.launch.py
+++ launch/cartographer.launch.py
@@ -82,7 +82,7 @@ def generate_launch_description():
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            arguments=['-d', rviz_config_dir],
+            # arguments=['-d', rviz_config_dir],
             parameters=[{'use_sim_time': use_sim_time}],
             output='screen'),
     ])
